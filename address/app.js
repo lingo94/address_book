c06e6d95e6950caf171e839276803686354dabc9
lingo94 <khj99435@naver.com>
2020-07-06 23:26:13 +0900

    Update
    mysql, bodyParser setting

--- address/app.js
+++ address/app.js
@@ -3,11 +3,22 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mysql = require('mysql');
+var bodyParser = require('body-parser');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+// DB setting
+var db = mysql.createConnection({
+  host : 'localhost',
+  user  : 'root',
+  password : 'q1w2e3r4',
+  database : 'board'
+});
+db.connect();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -18,6 +29,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// body-parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
